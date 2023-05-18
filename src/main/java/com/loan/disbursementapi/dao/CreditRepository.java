d829ec0d2fee40615116458374ff0db5e01aec5e
mco <muslumcanozata@gmail.com>
2023-05-18 13:22:02 +0300

    fix

--- src/main/java/com/loan/disbursementapi/dao/CreditRepository.java
+++ src/main/java/com/loan/disbursementapi/dao/CreditRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CreditRepository extends JpaRepository<Credit, int> {
+public interface CreditRepository extends JpaRepository<Credit, Integer> {
 
 }
