1e5767d1b0c144a09b1d19d583503570c414db6d
mco <muslumcanozata@gmail.com>
2023-05-23 16:39:17 +0300

    increasing interest rate to see difference

--- src/main/java/com/loan/disbursementapi/domain/constant/Constants.java
+++ src/main/java/com/loan/disbursementapi/domain/constant/Constants.java
@@ -7,6 +7,6 @@ public class Constants {
     public static final Integer ZERO = 0;
     public static final Integer ONE = 1;
     public static final Integer TWO = 2;
-    public static final BigDecimal INTEREST_RATE = BigDecimal.valueOf(0.01);
+    public static final BigDecimal INTEREST_RATE = BigDecimal.valueOf(0.5);
     public static final Integer DAY_ON_ONE_YEAR = 360;
 }
